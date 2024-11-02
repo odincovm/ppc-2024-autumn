6779c52a6d8cad17b633119f3625384eb3a7fc4a
Rerard <odmisa937@gmail.com>
2024-11-02 13:19:02 +0300

    add tests mpi

--- tasks/mpi/Odintsov_M_CountingMismatchedCharactersStr/func_tests/main.cpp
+++ tasks/mpi/Odintsov_M_CountingMismatchedCharactersStr/func_tests/main.cpp
@@ -132,3 +132,43 @@ TEST(Parallel_MPI_count, df_sz_13) {
     ASSERT_EQ(out[0], out_s[0]);
   }
 }
+TEST(Parallel_MPI_count, df_sz_27) {
+  // Create data//
+  boost::mpi::communicator com;
+  char str1[] = "lsjgjeqwqjfiijsnbhjfwfejfeswee";
+  char str2[] = "qellowhwmvptwreqqgrterqq";
+
+  std::vector<char*> in{str1, str2};
+  std::vector<int> out(1, 1);
+  std::vector<int> out_s(1, 1);
+  // Create Task Data Parallel
+  std::shared_ptr<ppc::core::TaskData> taskDataPar = std::make_shared<ppc::core::TaskData>();
+  if (com.rank() == 0) {
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(in[0]));
+    taskDataPar->inputs.emplace_back(reinterpret_cast<uint8_t*>(in[1]));
+    taskDataPar->inputs_count.emplace_back(in.size());
+    taskDataPar->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+    taskDataPar->outputs_count.emplace_back(out.size());
+  }
+
+  Odintsov_M_CountingMismatchedCharactersStr_mpi::CountingCharacterMPIParallel testClassPar(taskDataPar);
+  ASSERT_EQ(testClassPar.validation(), true);
+  testClassPar.pre_processing();
+  testClassPar.run();
+  testClassPar.post_processing();
+
+  if (com.rank() == 0) {
+    std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in[0]));
+    taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in[1]));
+    taskDataSeq->inputs_count.emplace_back(in.size());
+    taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out_s.data()));
+    taskDataSeq->outputs_count.emplace_back(out_s.size());
+    Odintsov_M_CountingMismatchedCharactersStr_mpi::CountingCharacterMPISequential testClassSeq(taskDataSeq);
+    ASSERT_EQ(testClassSeq.validation(), true);
+    testClassSeq.pre_processing();
+    testClassSeq.run();
+    testClassSeq.post_processing();
+    ASSERT_EQ(out[0], out_s[0]);
+  }
+}
