011b8747c9c3822ee313bcf47d2d19a9d56d9f78
Rerard <odmisa937@gmail.com>
2024-12-21 19:53:42 +0300

    add tests

--- tasks/seq/Odintsov_M_CountingMismatchedCharactersStr/func_tests/main.cpp
+++ tasks/seq/Odintsov_M_CountingMismatchedCharactersStr/func_tests/main.cpp
@@ -5,7 +5,7 @@
 
 #include "seq/Odintsov_M_CountingMismatchedCharactersStr/include/ops_seq.hpp"
 
-TEST(Sequential_count, ans_8) {
+TEST(Odintsov_m_Sequential_count, ans_8) {
   // Create data
 
   char str1[] = "qwert";
@@ -32,7 +32,7 @@ TEST(Sequential_count, ans_8) {
   ASSERT_EQ(8, out[0]);
 }
 
-TEST(Sequential_count, ans_0) {
+TEST(Odintsov_m_Sequential_count, ans_0) {
   // Create data
   char str1[] = "qwert";
   char str2[] = "qwert";
@@ -56,7 +56,7 @@ TEST(Sequential_count, ans_0) {
 
   ASSERT_EQ(0, out[0]);
 }
-TEST(Sequential_count, ans_10) {
+TEST(Odintsov_m_Sequential_count, ans_10) {
   // Create data
   char str1[] = "qwert";
 
@@ -82,7 +82,7 @@ TEST(Sequential_count, ans_10) {
 
   ASSERT_EQ(10, out[0]);
 }
-TEST(Sequential_count, ans_11) {
+TEST(Odintsov_m_Sequential_count, ans_11) {
   // Create data
   char str1[] = "qwerta";
   char str2[] = "asdfg";
@@ -105,4 +105,54 @@ TEST(Sequential_count, ans_11) {
   testClass.run();
   testClass.post_processing();
   ASSERT_EQ(11, out[0]);
+}
+
+TEST(Odintsov_m_Sequential_count, ans_22) {
+  // Create data
+  char str1[] = "qwertaqvsdfemm";
+  char str2[] = "asdfgsfefgfsmm";
+
+  std::vector<char*> in{str1, str2};
+  std::vector<int> out(1, 1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in[0]));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in[1]));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  Odintsov_M_CountingMismatchedCharactersStr_seq::CountingCharacterSequential testClass(taskDataSeq);
+  ASSERT_EQ(testClass.validation(), true);
+  testClass.pre_processing();
+  testClass.run();
+  testClass.post_processing();
+  ASSERT_EQ(22, out[0]);
+}
+
+TEST(Odintsov_m_Sequential_count, ans_46) {
+  // Create data
+  char str1[] = "qwertaqvsdfemmqwfweegwegre";
+  char str2[] = "asdfgsfefgfsmmfbthjykukiyl";
+
+  std::vector<char*> in{str1, str2};
+  std::vector<int> out(1, 1);
+
+  // Create TaskData
+  std::shared_ptr<ppc::core::TaskData> taskDataSeq = std::make_shared<ppc::core::TaskData>();
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in[0]));
+  taskDataSeq->inputs.emplace_back(reinterpret_cast<uint8_t*>(in[1]));
+  taskDataSeq->inputs_count.emplace_back(in.size());
+  taskDataSeq->outputs.emplace_back(reinterpret_cast<uint8_t*>(out.data()));
+  taskDataSeq->outputs_count.emplace_back(out.size());
+
+  // Create Task
+  Odintsov_M_CountingMismatchedCharactersStr_seq::CountingCharacterSequential testClass(taskDataSeq);
+  ASSERT_EQ(testClass.validation(), true);
+  testClass.pre_processing();
+  testClass.run();
+  testClass.post_processing();
+  ASSERT_EQ(46, out[0]);
 }
