1a5566b6dac3649d41f9da031d4c9d782b47498b
Tanya Yasakova <113035646+Tanya-Yasakova@users.noreply.github.com>
2024-12-23 01:57:45 +0300

    Ясакова Татьяна. Задача 2. Вариант 6. Передача от всеx одному (reduce) (#535)
    Реализована функция reduce, которая используется для решения задачи
    нахождения минимального элемента в матрице.
    
    Процесс работы следующий:
    1. Корневой процесс разбивает исходную матрицу на равные части.
    2. Каждый процесс получает свою часть матрицы и находит минимум в ней,
    используя функцию CustomReduce.
    3. Функция CustomReduce реализована по принципу двоичного дерева.
    Начиная с листовых процессов (которые обрабатывают отдельные части
    данных), процессы постепенно объединяются со своими соседями.
    4. На каждом шаге два объединяющихся процесса сливают свои промежуточные
    результаты (найденные минимумы), пока не останется одно итоговое
    минимальное значение, которое будет у корневого процесса.
    
    Таким образом, распараллеливая обработку матрицы и используя
    иерархическое объединение промежуточных результатов, корневой процесс
    получает минимальный элемент всей матрицы.

---
+++ tasks/mpi/yasakova_t_reduce/include/ops_mpi.hpp
@@ -0,0 +1,32 @@
+// Copyright 2023 Nesterov Alexander
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <boost/mpi/collectives.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <memory>
+#include <numeric>
+#include <utility>
+#include <vector>
+
+#include "core/task/include/task.hpp"
+
+namespace yasakova_t_reduce {
+
+class TestMPITaskParallel : public ppc::core::Task {
+ public:
+  explicit TestMPITaskParallel(std::shared_ptr<ppc::core::TaskData> taskData_) : Task(std::move(taskData_)) {}
+  bool pre_processing() override;
+  bool validation() override;
+  bool run() override;
+  bool post_processing() override;
+
+ private:
+  std::vector<int> input_data_, local_input_data_;
+  int result_{};
+  int columns, rows;
+  boost::mpi::communicator world;
+};
+
+}  // namespace yasakova_t_reduce
