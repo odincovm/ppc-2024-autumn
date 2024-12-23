36180dd4b42defbb1cf5853a95e55fe20bfd708b
Alexander <87474596+kobych@users.noreply.github.com>
2024-12-23 03:56:47 +0300

    Зиновьев Александр Задача 2 Вариант 2 Задача «Читатели-Писатели» (#627)
    В данной работе представлена реализация классической задачи
    синхронизации "Читатели-Писатели" в контексте параллельного
    программирования. Решение предоставлено как в параллельной версии с
    использованием MPI, так и в последовательной версии. Реализация включает
    в себя функциональные тесты, тесты производительности, а также
    необходимые исходные и заголовочные файлы.
    
    Проект содержит два основных класса задач:
    
    **ReadersWritersMPITaskSequential:** Класс, реализующий последовательное
    выполнение задачи "Читатели-Писатели". Используется для сравнения с
    параллельным выполнением.
    
    **ReadersWritersMPITaskParallel:** Класс, реализующий параллельное
    выполнение задачи "Читатели-Писатели" с использованием MPI. Распределяет
    вычисления на несколько процессов и использует механизмы синхронизации
    для обеспечения корректного доступа к общим данным.

---
+++ tasks/mpi/zinoviev_a_readers_and_writers/src/ops_mpi.cpp
@@ -0,0 +1,96 @@
+#include "mpi/zinoviev_a_readers_and_writers/include/ops_mpi.hpp"
+
+#include <algorithm>
+#include <functional>
+#include <random>
+#include <string>
+#include <thread>
+#include <vector>
+
+using namespace std::chrono_literals;
+
+bool zinoviev_a_readers_and_writers_mpi::ReadersWritersMPITaskSequential::pre_processing() {
+  internal_order_test();
+  // Init vectors
+  input_ = std::vector<int>(taskData->inputs_count[0]);
+  auto* tmp_ptr = reinterpret_cast<int*>(taskData->inputs[0]);
+  for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
+    input_[i] = tmp_ptr[i];
+  }
+  // Init value for output
+  res = 0;
+  return true;
+}
+
+bool zinoviev_a_readers_and_writers_mpi::ReadersWritersMPITaskSequential::validation() {
+  internal_order_test();
+  // Check count elements of output
+  return (taskData->outputs_count[0] == 1 && taskData->inputs_count[0] >= 0);
+}
+
+bool zinoviev_a_readers_and_writers_mpi::ReadersWritersMPITaskSequential::run() {
+  internal_order_test();
+  res = std::accumulate(input_.begin(), input_.end(), 0);
+  return true;
+}
+
+bool zinoviev_a_readers_and_writers_mpi::ReadersWritersMPITaskSequential::post_processing() {
+  internal_order_test();
+  reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
+  return true;
+}
+
+bool zinoviev_a_readers_and_writers_mpi::ReadersWritersMPITaskParallel::pre_processing() {
+  internal_order_test();
+  unsigned int delta = 0;
+  if (world.rank() == 0) {
+    delta = taskData->inputs_count[0] / world.size();
+    std::cout << "Delta: " << delta << std::endl;
+  }
+  broadcast(world, delta, 0);
+
+  if (world.rank() == 0) {
+    // Init vectors
+    input_ = std::vector<int>(taskData->inputs_count[0]);
+    auto* tmp_ptr = reinterpret_cast<int*>(taskData->inputs[0]);
+    for (unsigned i = 0; i < taskData->inputs_count[0]; i++) {
+      input_[i] = tmp_ptr[i];
+    }
+    for (int proc = 1; proc < world.size(); proc++) {
+      world.send(proc, 0, input_.data() + proc * delta, delta);
+    }
+  }
+  local_input_ = std::vector<int>(delta);
+  if (world.rank() == 0) {
+    local_input_ = std::vector<int>(input_.begin(), input_.begin() + delta);
+  } else {
+    world.recv(0, 0, local_input_.data(), delta);
+  }
+  // Init value for output
+  res = 0;
+  return true;
+}
+
+bool zinoviev_a_readers_and_writers_mpi::ReadersWritersMPITaskParallel::validation() {
+  internal_order_test();
+  if (world.rank() == 0) {
+    // Check count elements of output
+    return (taskData->outputs_count[0] == 1 && taskData->inputs_count[0] >= 0);
+  }
+  return true;
+}
+
+bool zinoviev_a_readers_and_writers_mpi::ReadersWritersMPITaskParallel::run() {
+  internal_order_test();
+  int local_res = std::accumulate(local_input_.begin(), local_input_.end(), 0);
+  reduce(world, local_res, res, std::plus(), 0);
+  return true;
+}
+
+bool zinoviev_a_readers_and_writers_mpi::ReadersWritersMPITaskParallel::post_processing() {
+  internal_order_test();
+  if (world.rank() == 0) {
+    reinterpret_cast<int*>(taskData->outputs[0])[0] = res;
+  }
+  return true;
+}
